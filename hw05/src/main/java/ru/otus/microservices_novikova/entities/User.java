8e33da4ee7690f522a9bae3656d458300623e2cc
alexaNovikova <alexadubinina@mail.ru>
2025-06-16 13:35:12 +0700

    ДЗ 5 - Prometheus, Grafana

---
+++ hw05/src/main/java/ru/otus/microservices_novikova/entities/User.java
@@ -0,0 +1,35 @@
+package ru.otus.microservices_novikova.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "users")
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+}
